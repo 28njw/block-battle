b6c9e64bc18184659e800e5454c02a405af3f142
Noah White <njwhite@umass.edu>
2022-07-06 09:13:10 -0400

    added front end to start game, rotating pieces logic in index.js and board.js

--- board.js
+++ board.js
@@ -1,12 +1,12 @@
-class Board{
-    constructor(){
+class Board {
+    constructor() {
         //create an empty board
         this.state = new Array(20).fill(0).map(() => new Array(10).fill(0));
         //holds the active tetris piece
-        this.activePiece = {pos: {x: 0, y: 0}, shape: null};
+        this.activePiece = { pos: { x: 0, y: 0 }, shape: null };
         this.canvas = document.getElementById('board');
         this.context = this.canvas.getContext('2d');
-        this.context.scale(20, 20);
+        this.context.scale(40, 40);
         this.colors = [
             null,
             '#F07167',
@@ -16,55 +16,56 @@ class Board{
             '#B1CC74',
             '#FF4000',
             '#FF7E6B',
-          ];
+        ];
         this.pieces = {
             I: [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
             ],
             L: [
-            [0, 2, 0],
-            [0, 2, 0],
-            [0, 2, 2],
+                [0, 2, 0],
+                [0, 2, 0],
+                [0, 2, 2],
             ],
             J: [
-            [0, 3, 0],
-            [0, 3, 0],
-            [3, 3, 0],
+                [0, 3, 0],
+                [0, 3, 0],
+                [3, 3, 0],
             ],
             O: [
-            [4, 4],
-            [4, 4],
+                [4, 4],
+                [4, 4],
             ],
             Z: [
-            [5, 5, 0],
-            [0, 5, 5],
-            [0, 0, 0],
+                [5, 5, 0],
+                [0, 5, 5],
+                [0, 0, 0],
             ],
             S: [
-            [0, 6, 6],
-            [6, 6, 0],
-            [0, 0, 0],
+                [0, 6, 6],
+                [6, 6, 0],
+                [0, 0, 0],
             ],
             T: [
-            [0, 7, 0],
-            [7, 7, 7],
-            [0, 0, 0],
-            ]};
+                [0, 7, 0],
+                [7, 7, 7],
+                [0, 0, 0],
+            ]
+        };
     }
 
     //redraw the board state within the given element
-    render(element){
+    render(element) {
         this.context.fillStyle = '#000000';
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.drawNextTick(this.state, {x: 0, y: 0});
-        this.drawNextTick(this.activePiece.shape, this.activePiece.pos); 
+        this.drawNextTick(this.state, { x: 0, y: 0 });
+        this.drawNextTick(this.activePiece.shape, this.activePiece.pos);
     }
 
     //update and redraw the board for the next "tick" of time
-    drawNextTick(state, pos){
+    drawNextTick(state, pos) {
         state.forEach((row, y) => {
             row.forEach((value, x) => {
                 if (value !== 0) {
@@ -110,16 +111,16 @@ class Board{
     //merge current piece to board
     mergeToBoard() {
         this.activePiece.shape.forEach((row, y) => {
-          row.forEach((value, x) => {
-            if (value !== 0) {
-              this.state[y + this.activePiece.pos.y][x + this.activePiece.pos.x] = value;
-            }
-          });
+            row.forEach((value, x) => {
+                if (value !== 0) {
+                    this.state[y + this.activePiece.pos.y][x + this.activePiece.pos.x] = value;
+                }
+            });
         });
     }
 
     //rotate active piece
-    rotate(){
+    rotate() {
         let prevOrientation = this.activePiece.shape;
         let newshape = [];
         for (let j = 0; j < this.activePiece.shape.length; ++j) {
@@ -129,27 +130,27 @@ class Board{
             }
         }
         this.activePiece.shape = newshape;
-        if(this.collision()){
+        if (this.collision()) {
             this.activePiece.shape = prevOrientation;
-        } else{
+        } else {
             this.render();
         }
     }
 
     //check if game is lost
-    failGame(){
+    failGame() {
         //TODO: end game if piece is merged outside of bounds or into another
     }
 
     checkFilledRows() {
         check: for (let y = this.state.length - 1; y > 0; --y) {
-          for (let x = 0; x < this.state[y].length; ++x) {
-            if (this.state[y][x] === 0) {
-              continue check;
+            for (let x = 0; x < this.state[y].length; ++x) {
+                if (this.state[y][x] === 0) {
+                    continue check;
+                }
             }
-          }
-          this.state.splice(y, 1);
-          this.state.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+            this.state.splice(y, 1);
+            this.state.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
         }
     }
 }
--- index.js
+++ index.js
@@ -2,36 +2,160 @@ import { Board } from './board.js';
 const board = new Board();
 let playing = false;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 document.addEventListener('keydown', event => {
-    if(playing){
+    if (playing) {
         if (event.keyCode === 37) {
-            board.activePiece.pos.x -= 1;
+            board.activePiece.pos.x--;
             if (board.collision()) {
-              board.activePiece.pos.x += 1;
+                board.activePiece.pos.x++;
             }
-          } else if (event.keyCode === 39) {
-            board.activePiece.pos.x += 1;
+        } else if (event.keyCode === 39) {
+            board.activePiece.pos.x++;
             if (board.collision()) {
-              board.activePiece.pos.x -= 1;
+                board.activePiece.pos.x -= 1;
             }
-          } else if (event.keyCode === 40) {
-            board.activePiece.pos.y += 1;
+        } else if (event.keyCode === 40) {
+            board.activePiece.pos.y++;
             if (board.collision()) {
-              board.activePiece.pos.y -= 1;
+                board.activePiece.pos.y--;
             }
-          } else if (event.keyCode === 38) {
-              board.rotate();
-          }
+        } else if (event.keyCode === 38) {
+            board.rotate();
+        }
     }
 });
 
 document.getElementById('play-game').addEventListener('click', event => {
     playing = true;
-    board.createNewPiece();
-    window.setInterval(() => {
-        board.render();
-    }, 50);
-    window.setInterval(() => {
-        board.fall();
-    }, 500);
+    glitch(document.getElementById("interactiveButtons"), 7);
+    fadeOut(document.getElementById("interactiveButtons"));
+    glitch(document.getElementById("mainHeader"), 13);
+    fadeOut(document.getElementById("mainHeader"));
+    window.setTimeout(() => {
+        document.getElementById("mainHeader").remove();
+        document.getElementById("interactiveButtons").remove();
+        document.getElementById('board').hidden = false;
+        document.getElementById('board').classList.add("fadeIn");
+        board.createNewPiece();
+        window.setInterval(() => {
+            board.render();
+        }, 50);
+        window.setInterval(() => {
+            board.fall();
+        }, 500);
+    }, 1000)
+
 });
+
+function buildBackground() {
+    let rate = Math.round(Math.max((screen.width / 66) / 2 + 2, (screen.height / 66) / 2 + 2));
+    rate < 14 ? rate = 14 : rate = rate;
+    function buildRight() {
+        let c = 4;
+        let html = '';
+        let tops = 2 * -66;
+        let left = -66;
+        for (let i = 1; i < rate; ++i) {
+            for (let p = 1; p < c; ++p) {
+                (p > c / 2) ? (left -= (40), tops += (66)) : (p != 1 ? (left += 40, tops += 66) : (left += i * 40));
+                p == c / 2 ? tops = (-66) : tops = tops;
+                if (Math.abs(tops) * 2 < screen.height + 66 && Math.abs(left) * 2 < screen.width + 66) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>'
+                }
+                else if (screen.width < 600) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>'
+                }
+            }
+            tops = (i + 2) * -66;
+            left = (-66);
+            c += 2;
+            html += '<div></div>';
+
+        }
+        return html;
+    }
+    function buildLeft() {
+        let html = '';
+        let c = 4;
+        let tops = 2 * -66;
+        let left = -66;
+        for (let i = 1; i < rate; ++i) {
+            for (let p = 1; p < c; ++p) {
+                (p > c / 2) ? (left += (40), tops += (66)) : (p != 1 ? (left -= 40, tops += 66) : (left -= i * 40));
+                p == c / 2 ? tops = (-66) : tops = tops;
+                if (Math.abs(tops) * 2 < screen.height + 66 && Math.abs(left) * 2 < screen.width + 66) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>'
+                }
+                else if (screen.width < 600) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>'
+                }
+            }
+            tops = (i + 2) * -66;
+            left = (-66);
+            c += 2;
+            html += '<div></div>';
+        }
+        return html;
+    }
+
+    function buildTopBot() {
+        let html = '';
+        let c = 2;
+        let tops = 66;
+        let left = -66;
+        for (let i = 1; i < rate; ++i) {
+            for (let p = 1; p < c; ++p) {
+                if (Math.abs(tops) * 2 < screen.height + 66 && Math.abs(left) * 2 < screen.width + 66) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>';
+                    left += 80
+                }
+                else if (screen.width < 600) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>';
+                    left += 80
+                }
+            }
+            left = -27 + ((i + 1) * -40);
+            tops += 66;
+            c += 1;
+            html += '<div></div>';
+        }
+        c = 2;
+        tops = -66;
+        left = -66;
+        for (let i = 1; i < rate; ++i) {
+            for (let p = 1; p < c; ++p) {
+                if (Math.abs(tops) * 2 < screen.height + 66 && Math.abs(left) * 2 < screen.width + 66) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>';
+                    left += 80
+                }
+                else if (screen.width < 600) {
+                    html += '<hex style="' + 'left:calc(50% + (' + String(left) + 'px));top:calc(50% + (' + String(tops) + 'px)); -webkit-animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite; animation:opacity 2s ' + i * 0.25 + 's ease-in-out infinite;"></hex>';
+                    left += 80
+                }
+            }
+            left = -27 + ((i + 1) * -40);
+            tops -= 66;
+            c += 1;
+            html += '<div></div>';
+        }
+        return html;
+    }
+    document.getElementById('hex').innerHTML = '<hex></hex>' + buildRight() + buildLeft() + buildTopBot();
+}
+
+buildBackground();
+
+async function glitch(el, len) {
+    for (let i = 0; i < len; ++i) {
+        el.style.fontFamily = "heading" + String((i % 2));
+        await sleep(Math.random() * (100 - 25) + 25);
+    }
+}
+function fadeOut(element) {
+    element.classList.add("fadeOut");
+}
+
