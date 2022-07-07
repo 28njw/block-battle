e6b8735050c686a0d31575db66396f3b63ad2d17
Noah White <njwhite@umass.edu>
2022-07-07 14:47:11 -0400

    finished database class

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const { Pool } = pg;
 export class Database {
     constructor(){
         this.url = 'postgres://cuegmbdolvultw:f3a643ef3a23cff218f4f7ae7fcf7d7edf8246d5c152b51228c84f687ea8474d@ec2-54-159-22-90.compute-1.amazonaws.com:5432/dfja7ah0kkcic0';
+        this.leaderboard = this.getTopTen();
     }
 
     async connect(){
@@ -26,13 +27,22 @@ export class Database {
         const queryText = 'SELECT TOP 10 score FROM scores ORDER BY score DESC;';
         const res = await this.client.query(queryText, [username]);
         console.log(res);
+        this.leaderboard = res.rows;
         return res.rows;
     }
 
     //write a score entry to database
     submitScore(entry){
-        const queryText = `INSERT INTO scores (username, score) VALUES (${entry.username}, ${entry.score})`;
-        const res = await this.client.query(queryText, [username]);
-        return res.rows;
+        let min = Infinity;
+        for(let i = 0; i < this.leaderboard.length; ++i){
+            if(this.leaderboard[i].score < min){
+                min = this.leaderboard[i].score;
+            }
+        }
+        if(entry.score > min){
+            const queryText = `INSERT INTO scores (username, score) VALUES (${entry.username}, ${entry.score})`;
+            const res = await this.client.query(queryText, [username]);
+            return res.rows;
+        }
     }
 }
